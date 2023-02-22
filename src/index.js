e5cf03b364d8ee0ae61d410384933a4980064740
Maksim Katrich <mxmktr@gmail.com>
2023-02-22 23:36:10 +0200

    add functional to button!

--- src/index.js
+++ src/index.js
@@ -2,26 +2,42 @@
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix'; */
 import axios from 'axios';
+import ApiService from './js/ApiService';
 import './css/styles.css';
 
-const URL = 'https://pixabay.com/api/?';
-
-const params = {
-  key: '7900432-26a61502dc2db9573bbff3077',
-  /* q: '', */
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  page: 1,
-  per_page: 40,
-};
-
-const searchParams = new URLSearchParams(params);
-axios.get(`${URL}${searchParams}`).then(responseFromApi);
-
-function responseFromApi(response) {
-  console.log(response);
-}
-
 const form = document.getElementById('search-form');
+const submitBtn = document.querySelector('.search-form button');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+
+const apiService = new ApiService();
+
+let searchQueryValue = '';
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const currentForm = event.currentTarget;
+
+  searchQueryValue = currentForm.elements.searchQuery.value.trim();
+
+  if (!searchQueryValue) {
+    console.log('Idiot, you fill nothing!');
+    return;
+  }
+
+  apiService
+    .getImages(searchQueryValue)
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message));
+});
+
+loadBtn.addEventListener('click', event => {
+  apiService.nextPage();
+  if (apiService.numberOfPages > apiService.params.page) {
+    apiService.getImages(searchQueryValue).then(data => console.log(data));
+  } else {
+    apiService.getImages(searchQueryValue).then(data => console.log(data));
+    loadBtn.disabled = true;
+    loadBtn.textContent = 'Finish';
+  }
+});
---
+++ src/js/ApiService.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export default class ApiService {
+  URL = 'https://pixabay.com/api/?';
+
+  params = {
+    key: '7900432-26a61502dc2db9573bbff3077',
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  };
+
+  constructor() {}
+
+  getImages(query) {
+    this.params.q = query;
+    return axios
+      .get(`${this.URL}${new URLSearchParams(this.params)}`)
+      .then(response => {
+        return response.data;
+      })
+      .then(data => {
+        const totalElements = data.totalHits;
+
+        if (totalElements === 0) {
+          throw new Error(
+            "You are crazy! We don't have pictures with this name!"
+          );
+        }
+
+        this.numberOfPages = Math.ceil(totalElements / this.params.per_page);
+        return data.hits;
+      });
+  }
+
+  nextPage() {
+    this.params.page += 1;
+  }
+}
